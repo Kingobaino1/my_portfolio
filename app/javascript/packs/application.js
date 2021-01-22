3c2faeda1ba571713fcae93dfdaf0d51b01dc9b0
Kingsley <kingobaino@gmail.com>
2021-01-23 00:45:19 +0100

    Style about me section

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,15 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-import "bootstrap";
+
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
 
-
+import "bootstrap";
 import "../stylesheets/application";
-
 import "@fortawesome/fontawesome-free/js/all";
 document.addEventListener(
   "turbolinks:load",
@@ -28,5 +27,5 @@ document.addEventListener(
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
